deeaf13ab9c747899a896ed3b100c7cc64df5ed5
fkzys <fkzys@proton.me>
2026-02-16 00:28:52 +0300

    nextcloud cron podman timer

--- nextcloud/deploy.py
+++ nextcloud/deploy.py
@@ -19,13 +19,19 @@ deployer = ServiceDeployer({
         ('4-nginx.container.j2', '/etc/containers/systemd/4-nginx.container'),
         ('config.php.j2', f'{REMOTE_BASE}/nextcloud/config/config.php', {'owner': '33:33'}),
         ('nginx.conf.j2', f'{REMOTE_BASE}/nginx.conf'),
+        ('nextcloud-cron.timer.j2', '/etc/systemd/system/nextcloud-cron_podman.timer'),
+        ('nextcloud-cron.service.j2', '/etc/systemd/system/nextcloud-cron_podman.service'),
     ],
     'setup_dirs': [
         f'{REMOTE_BASE}/nextcloud/config',
         f'{REMOTE_BASE}/db',
         f'{REMOTE_BASE}/log',
     ],
-    'restart_cmd': 'systemctl daemon-reload && systemctl restart nextcloud-pod',
+    'restart_cmd': (
+        'systemctl daemon-reload && '
+        'systemctl restart nextcloud-pod && '
+        'systemctl enable --now nextcloud-cron_podman.timer'
+    ),
 })
 
 if __name__ == '__main__':
